cf261ea8a6b610e6d91a876e0d46dc6e995a4bda
Alexander <101361799+Almaze3@users.noreply.github.com>
2022-12-09 18:13:13 +0300

    Pr11_fix
    Настроены респонзы

--- djangoProject9/api/views.py
+++ djangoProject9/api/views.py
@@ -4,11 +4,14 @@ from .models import Mail
 from .serializers import MailSerializer
 import nickname
 from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+
 
 # Create your views here.
 
 
 class MailView(APIView):
+    @extend_schema(request=MailSerializer, responses=MailSerializer)
 
     def get(self, request, mail):
         name = Mail(mail, nickname.nickname(mail))
@@ -16,3 +19,5 @@ class MailView(APIView):
         serializer_for_request = MailSerializer(instance=name)
 
         return Response(serializer_for_request.data)
+
+
--- djangoProject9/djangoProject9/urls.py
+++ djangoProject9/djangoProject9/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 from api import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('func_app/', include('func_app.urls'))
+    path('func_app/', include('func_app.urls')),
+    path('schema/', SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui",
+    ),
 ]
